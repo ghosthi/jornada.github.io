a8e9ad876ee6ccc64ef8178a19b2dc60222159ee
ghosthi <thiago.2020@alunos.utfpr.edu.br>
2024-06-09 19:17:19 -0300

    semifinal website

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-$(function(){
+ $(function(){
+    startTimer(60*30, $(".countdown-btn"));
     $(".collapsible-header").on("click", function(){
         let content = $(this).siblings(".collapsible-content");
         var isVisible = content.css("height") == "0px";
@@ -6,4 +7,28 @@ $(function(){
         if(isVisible)
             content.animate({height: content.get(0).scrollHeight},5);
     });
+    
+    let data = new Date();
+    $(".header-text > span").html(
+        (data.getDate() - 2) + ", " + (data.getDate() - 1) + " e " + data.getDate() + " de " + 
+        ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"][data.getMonth()]
+    );
+    
 });
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.html(`<i class="material-icons">flash_on</i> ${minutes}:${seconds}`);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
